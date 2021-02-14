552ec2acf50515cac29897b3f15b8d36f2392e95
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-02-14 02:12:33 +0100

    Fear change login view

---
+++ layouts/login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login--layout">
+    <header class="login--header">
+      <figure>
+        <Logo />
+        <figcaption class="logo--label">Champions<br />academy</figcaption>
+      </figure>
+    </header>
+    <main class="login--box">
+      <Nuxt />
+    </main>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 20px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+.login--layout {
+  background-color: #003366;
+  color: #fff;
+  min-height: 100vh;
+  padding-top: 3rem;
+}
+.login--header {
+  display: flex;
+  justify-content: center;
+}
+.login--box {
+  display: flex;
+  justify-content: center;
+  padding-top: 2rem;
+}
+.logo--label {
+  display: flex;
+  font-size: 1.5rem;
+  justify-content: flex-end;
+  text-transform: uppercase;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,25 +1,28 @@
 <template>
-  <div>
-    <h1>Log In</h1>
-    <form @submit.prevent="submit">
-      <p v-if="alert">{{ alert.message }}</p>
-      <label class="login--label">
-        Email
-        <input label="Email" v-model="email" />
-      </label>
-      <label class=".login--label">
-        Password
-        <input label="Password" v-model="password" type="password" />
-      </label>
-      <button type="submit">
-        Log In
-      </button>
-    </form>
-  </div>
+  <form @submit.prevent="submit">
+    <!-- <p v-if="alert">{{ alert.message }}</p> -->
+    <label class="login--label">
+      <span class="login--label__input">Nazwa użytkownika</span>
+      <input label="Email" v-model="email" class="login--input" />
+    </label>
+    <label class="login--label">
+      <span class="login--label__input">Hasło</span>
+      <input
+        label="Password"
+        v-model="password"
+        type="password"
+        class="login--input"
+      />
+    </label>
+    <button type="submit" class="login--button">
+      Login
+    </button>
+    <a class="login--link" href="#">Zapomniałeś hasła?</a>
+  </form>
 </template>
 <script>
 export default {
-  layout: "fullscreen",
+  layout: "login",
   data() {
     return {
       email: "",
@@ -58,5 +61,35 @@ export default {
 <style scope>
 .login--label {
   display: block;
+  padding-bottom: 0.5rem;
+}
+.login--input {
+  border: none;
+  border-radius: 0.5rem;
+  display: block;
+  font-size: 1rem;
+  padding: 0.5rem;
+}
+.login--label__input {
+  display: block;
+  padding-bottom: 0.25rem;
+}
+.login--button {
+  border: none;
+  border-radius: 0.5rem;
+  background-color: #a4a4df;
+  display: flex;
+  font-size: 1rem;
+  margin-left: auto;
+  padding: 0.5rem;
+  text-transform: uppercase;
+}
+.login--link {
+  color: #5eb3c9;
+  text-decoration: none;
+}
+.login--button:hover {
+  background-color: #7e7ec7;
+  cursor: pointer;
 }
 </style>
