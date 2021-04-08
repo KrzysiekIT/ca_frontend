add626d87275e35e4691231f857bd1d832954f53
KrzysiekIT <krzysztof.karczewski@protonmail.com>
2021-04-08 16:44:24 +0200

    Feat add user payments

--- pages/student/account.vue
+++ pages/student/account.vue
@@ -16,12 +16,25 @@
     </div>
     <div class="account__box">
       <div class="account__box--inner">
-        <img
-          src="~/assets/images/user_white.svg"
-          alt="User icon white"
-          height="32"
-        />
-        Konto ucznia
+        <section class="payments">
+          <img src="~/assets/images/money.svg" alt="Sad icon" height="64" />
+          <img src="~/assets/images/sad.svg" alt="Sad icon" height="64" />
+          <div>
+            <label class="payments__label">STATUS</label>
+            <strong
+              class="payments__label-value payments__label-value--suspended"
+              >AKTYWNY</strong
+            >
+          </div>
+          <div class="payments__icon-label">
+            <img
+              src="~/assets/images/terms_of_use.svg"
+              alt="User icon white"
+              height="32"
+            />
+            <span>KONTO UCZNIA</span>
+          </div>
+        </section>
       </div>
       <div class="account__box--inner">abc</div>
       <div class="account__box--inner">abc</div>
@@ -38,6 +51,24 @@ $boxPadding: 0.5rem;
 .header__label-value {
   color: #00b0f0;
   padding-left: 1rem;
+}
+.header__label-value {
+  color: #00b0f0;
+  padding-left: 1rem;
+}
+.payments__label-value {
+  padding-left: 1rem;
+}
+.payments__icon-label {
+  align-items: center;
+  display: flex;
+  padding: $boxPadding;
+}
+.payments__label-value--active {
+  color: #00cc00;
+}
+.payments__label-value--suspended {
+  color: #ffff00;
 }
 .account__box {
   display: flex;
