81743cbb4224f90fac71b20619686fadc9142ed9
KrzysiekIT <krzysztof.karczewski@protonmail.com>
2021-04-09 14:24:45 +0200

    Style more BEM elements

--- pages/student/account.vue
+++ pages/student/account.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="account">
-    <div class="header">
+    <div class="account__header">
       <div>
-        <label class="header__label">Termin zajęć</label>
-        <strong class="header__label-value">Czwartek 18:00</strong>
+        <label class="account__header-label">Termin zajęć</label>
+        <strong>Czwartek 18:00</strong>
       </div>
       <div>
-        <label class="header__label">Trener</label>
-        <strong class="header__label-value">Imię Nazwisko</strong>
+        <label class="account__header-label">Trener</label>
+        <strong>Imię Nazwisko</strong>
       </div>
       <div>
-        <label class="header__label">Kontakt</label>
-        <strong class="header__label-value">500 600 500</strong>
+        <label class="account__header-label">Kontakt</label>
+        <strong>500 600 500</strong>
       </div>
     </div>
     <div class="account__box">
-      <section class="account__box--inner payments">
+      <section>
         <img src="~/assets/images/money.svg" alt="Sad icon" height="64" />
         <img src="~/assets/images/sad.svg" alt="Sad icon" height="64" />
         <div>
           <label class="payments__label">STATUS</label>
-          <strong class="payments__label-value payments__label-value--suspended"
-            >AKTYWNY</strong
+          <strong
+            class="payments__label-value payments__label-value--suspended"
           >
+            AKTYWNY
+          </strong>
         </div>
         <div class="payments__icon-label">
           <img
@@ -34,9 +36,9 @@
         </div>
       </section>
       <section class="account__box--inner year">
-        <ul>
-          <li>ROK 1</li>
-          <li>ROK 2</li>
+        <ul class="year__ul">
+          <li class="year__li">ROK 1</li>
+          <li class="year__li">ROK 2</li>
         </ul>
       </section>
       <section class="account__box--inner year">abc</section>
@@ -45,16 +47,12 @@
 </template>
 <style lang="scss" scoped>
 $boxPadding: 0.5rem;
-.header {
+.account__header {
   display: flex;
   justify-content: space-between;
   padding: $boxPadding;
 }
-.header__label-value {
-  color: #00b0f0;
-  padding-left: 1rem;
-}
-.header__label-value {
+.account__header-label {
   color: #00b0f0;
   padding-left: 1rem;
 }
@@ -72,11 +70,11 @@ $boxPadding: 0.5rem;
 .payments__label-value--suspended {
   color: #ffff00;
 }
-.year ul {
+.year__ul {
   list-style-type: none;
   padding: 0;
 }
-.year ul li:not(:first-child) {
+.year__li:not(:first-child) {
   padding: 0.75rem 0;
 }
 .account__box {
