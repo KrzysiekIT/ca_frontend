53d5b3e9360ab47ba5b7120f1df387bbb225a533
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-05-20 21:28:03 +0200

    add terms redirection

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,19 @@
+const areTermsAccepted = ({ store, route }) => {
+  const termsAccepted = store.state.auth?.user?.terms_accepted;
+  const roleName = store.state.auth?.user?.role?.name;
+  const studentPath = /^\/student(\/|$)/.test(route.fullPath);
+  const isTermsFirst = /^\/student\/terms-first(\/|$)/.test(route.fullPath);
+  if (
+    !termsAccepted &&
+    roleName === "student" &&
+    studentPath &&
+    !isTermsFirst
+  ) {
+    return false;
+  }
+  return true;
+};
+
 export default function({ store, redirect, route }) {
   const userIsLoggedIn = !!store.state.auth.user;
   const authLevel = {
@@ -7,11 +23,15 @@ export default function({ store, redirect, route }) {
   if (!userIsLoggedIn && authLevel.page) {
     return redirect("/login");
   }
-  if(!(authLevel.page)) {
+  if (!authLevel.page) {
     return Promise.resolve();
   }
   if (!(authLevel.page & authLevel.user)) {
     return redirect("/login");
+  }
+
+  if (!areTermsAccepted({ store, route })) {
+    return redirect("/student/terms-first");
   }
   /* const userIsLoggedIn = !!store.state.auth.user;
   const urlRequiresAuth = /^\/admin(\/|$)/.test(route.fullPath);
---
+++ pages/student/terms-first.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    ZAAKCEPTUJ TERMSY!!! {{user}}
+  </div>
+</template>
+
+<script>
+import user from "~/mixins/user.js";
+export default {
+  middleware: "auth",
+  meta: {
+    auth: { authority: 8 }
+  },
+  mixins: [user],
+  layout: "default"
+};
+</script>
